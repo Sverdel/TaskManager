ed1f4ff1bd6bc7f6f365870011c5d98a5bfc3d86
Sverdel Yurii <yu.sverdel@yandex.ru>
2016-06-07 20:26:07 +0500

    Create Database project. Some client fixes

---
+++ TaskManager.Client/app/controllerModal.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+       .module('taskApp')
+       .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, message) {
+
+        $scope.message = message;
+
+        $scope.ok = function () {
+            $uibModalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    });
+})();
---
+++ TaskManager.Database/dbo/Tables/States.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[States] (
+    [Id]   TINYINT       IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_dbo.States] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
